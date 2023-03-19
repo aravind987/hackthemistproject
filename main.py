1cd16a8732bbc91a175f30e4f9b72076b26c80c6
Irwin <irwinngo@gmail.com>
2023-03-18 20:37:10 -0400

    Completed character profile, added new Flask GET/POST function to check and retrieve info on account

--- main.py
+++ main.py
@@ -4,6 +4,21 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+
+'''
+Gets a username from the Front-End site and runs it through Co:here's API
+'''
+@app.route("/getAccount", methods=['GET','POST'])
+def checkAccount():
+    username = request.get_json()['username']
+    return [{
+        'image': "https://media.discordapp.net/attachments/757730257150279742/1080660938249736244/image.png?width=156&height=222",
+        'username': username,
+        'percentRacist': 0.4,
+        'percentHate': 0.4,
+        'percentNeutral': 0.2
+    }]
+
 @app.route("/getClass", methods=['GET','POST'])
 def classifyText():
     text_data = request.get_json()['keywords']
